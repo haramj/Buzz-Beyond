cffe04e3d3bb37e7e975ffe8ba8c104fd9d25409
haramjeong <04harams77@gmail.com>
2026-05-03 15:05:52 +0900

    fix: build error

--- next.config.js
+++ next.config.js
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false, 
   
   experimental: {
     workerThreads: false, 
     cpus: 1 
   },
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 }
 
 module.exports = nextConfig
